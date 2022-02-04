6e69f1073e6e43d770973d3b6d9493ca71d56525
SzabolcsGergely <szabi@luxonis.com>
2022-02-04 23:51:20 +0200

    Temp disable confidence map RGB alignment

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "22f00aeeec005de80c99084b23291a9f154bb84f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e7ab0e8612376efb9ed35e4555280bebbdf99572")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
